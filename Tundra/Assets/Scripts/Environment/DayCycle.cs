d4207059d470ba7b3ac4689cf2d5f224a3c586cd
taggeddev <denis.mikov.06@gmail.com>
2022-06-07 13:18:29 +0300

    code refactoring to unity conventions

--- Tundra/Assets/Scripts/Environment/DayCycle.cs
+++ Tundra/Assets/Scripts/Environment/DayCycle.cs
@@ -2,36 +2,40 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class DayCycle : MonoBehaviour
 {
     [Range(0, 1)]
-    public float CurrentTime;
-    public float DayDuration;
-    public Light Sun;
-    public Light Moon;
+    [SerializeField] private float currentTime;
+    
+    [FormerlySerializedAs("CurrentTime (s)")]
+    [SerializeField] private float dayDuration;
+    [SerializeField] private Light sun;
+    [SerializeField] private Light moon;
     
     private float _moonLightIntensity;
     private float _sunLightIntensity;
     
-    public AnimationCurve SunCurve;
-    public AnimationCurve MoonCurve;
+    public AnimationCurve sunCurve;
+    public AnimationCurve moonCurve;
 
     private void Start()
     {
-        _sunLightIntensity = Sun.intensity;
-        _moonLightIntensity = Moon.intensity;
+        currentTime = .2f;
+        _sunLightIntensity = sun.intensity;
+        _moonLightIntensity = moon.intensity;
     }
 
     private void Update()
     {
-        CurrentTime += Time.deltaTime / DayDuration;
-        if (CurrentTime >= 1) CurrentTime -= 1;
+        currentTime += Time.deltaTime / dayDuration;
+        if (currentTime >= 1) currentTime -= 1;
 
-        Sun.transform.localRotation = Quaternion.Euler(CurrentTime * 360f, 180, 0);
-        Moon.transform.localRotation = Quaternion.Euler(CurrentTime * 360f + 180f, 180, 0);
+        sun.transform.localRotation = Quaternion.Euler(currentTime * 360f, 180, 0);
+        moon.transform.localRotation = Quaternion.Euler(currentTime * 360f + 180f, 180, 0);
 
-        Sun.intensity = _sunLightIntensity * SunCurve.Evaluate(CurrentTime);
-        Moon.intensity = _moonLightIntensity * MoonCurve.Evaluate(CurrentTime);
+        sun.intensity = _sunLightIntensity * sunCurve.Evaluate(currentTime);
+        moon.intensity = _moonLightIntensity * moonCurve.Evaluate(currentTime);
     }
 }
