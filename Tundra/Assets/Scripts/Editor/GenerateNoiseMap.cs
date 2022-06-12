a36994c29af6fe6cfb1ec42927271d95085d38f5
taggeddev <denis.mikov.06@gmail.com>
2022-06-12 19:53:50 +0300

    feat(scripts): Implemented Perlin noise generation on plane

---
+++ Tundra/Assets/Scripts/Editor/GenerateNoiseMap.cs
@@ -0,0 +1,26 @@
+﻿using Environment;
+using UnityEditor;
+using UnityEngine;
+
+namespace Editor
+{
+    [CustomEditor(typeof(MapGenerator))]
+    public class GenerateNoiseMap : UnityEditor.Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            var mapGenerator = (MapGenerator)target;
+
+            if (DrawDefaultInspector())
+            {
+                if (mapGenerator.autoUpdate)
+                    mapGenerator.GenerateMap();
+            }
+
+            if (GUILayout.Button("Generate"))
+            {
+                mapGenerator.GenerateMap();
+            }
+        }
+    }
+}
---
+++ Tundra/Assets/Scripts/Environment/MapGenerator.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+namespace Environment
+{
+    public class MapGenerator : MonoBehaviour
+    {
+        private MapDisplay _display;
+    
+        [SerializeField] private int mapWidth;
+        [SerializeField] private int mapHeight;
+        [SerializeField] private float noiseScale;
+
+        public bool autoUpdate;
+
+        public void GenerateMap()
+        {
+            var noiseMap = Noise.GenerateNoiseMap(mapWidth, mapHeight, noiseScale);
+            _display = GetComponent<MapDisplay>();
+            _display.DrawNoiseMap(noiseMap);
+        }
+    
+    }
+}
