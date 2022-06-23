db8b923658fd191c4d1b5f5ac70a85c585439720
taggeddev <denis.mikov.06@gmail.com>
2022-06-23 09:50:00 +0300

    feat(script): new player states: health, starvation and temperature

---
+++ Tundra/Assets/Scripts/Player/Behaviour/PlayerHealth.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using UnityEngine;
+
+namespace Player.Behaviour
+{
+    public class PlayerHealth : MonoBehaviour
+    {
+        // Properties
+        public float MaxHealth
+        {
+            get => _maxHealth;
+            set => _maxHealth = value;
+        }
+        
+        // Fields
+        [SerializeField] private float _maxHealth;
+        
+        // Variables
+        private float currentHealth;
+        
+        // Public methods
+
+        // Private methods
+        private void Start()
+        {
+            currentHealth = _maxHealth;
+        }
+
+        private void Update()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Tundra/Assets/Scripts/Player/Behaviour/PlayerStarvation.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using UnityEngine;
+
+namespace Player.Behaviour
+{
+    public class PlayerStarvation : MonoBehaviour
+    {
+        // Properties
+        public float MaxStarve => maxStarve;
+
+        // Fields
+        [SerializeField] private float maxStarve;
+        [SerializeField] private float saturationTime;
+        
+        // Variables
+        private float currentStarveCapacity;
+        private float currentSaturationTime;
+        
+        // Public methods
+
+        // Private methods
+        private void Start()
+        {
+            currentStarveCapacity = maxStarve;
+            currentSaturationTime = saturationTime;
+        }
+
+        private void Update()
+        {
+            ContinueStarving();
+        }
+
+        private void ContinueStarving()
+        {
+            if (currentSaturationTime > 0)
+            {
+                currentSaturationTime -= Time.deltaTime;
+                return;
+            }
+            currentStarveCapacity -= 1;
+        }
+
+        private void ConsumeFood(float value)
+        {
+            if (currentStarveCapacity + value <= MaxStarve)
+                currentStarveCapacity += value;
+            else
+                currentStarveCapacity = MaxStarve;
+        }
+    }
+}
