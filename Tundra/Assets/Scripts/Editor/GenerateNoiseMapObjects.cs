39f6baa49925c6bcbe431b2c7caadc1946cd2331
taggeddev <denis.mikov.06@gmail.com>
2022-06-30 11:59:24 +0300

    feat(script): Created noise generator for objects

---
+++ Tundra/Assets/Scripts/Editor/GenerateNoiseMapObjects.cs
@@ -0,0 +1,27 @@
+﻿
+using Environment.Objects;
+using UnityEditor;
+using UnityEngine;
+
+namespace Editor
+{
+    [CustomEditor(typeof(ObjectMapGenerator))]
+    public class GenerateNoiseMapObjects : UnityEditor.Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            var mapGenerator = (ObjectMapGenerator)target;
+
+            if (DrawDefaultInspector())
+            {
+                if (mapGenerator.AutoUpdate)
+                    mapGenerator.DrawMapInEditor();
+            }
+
+            if (GUILayout.Button("Generate"))
+            {
+                mapGenerator.DrawMapInEditor();
+            }
+        }
+    }
+}
