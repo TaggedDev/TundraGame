ab211be03e95eac41e65b9c7739b54c52806cfc9
taggeddev <denis.mikov.06@gmail.com>
2022-07-01 16:43:10 +0300

    feat(script): Created generated objects

---
+++ Tundra/Assets/Entities/Environment/Nature/TreeEntity.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace ScriptableObjects.Environment.Nature
+{
+    public class TreeEntity : GeneratedObject
+    {
+        [SerializeField] private float health;
+        [SerializeField] private GameObject model;
+
+        public TreeEntity(Transform parent)
+        {
+            transform.parent = parent;
+        }
+    }
+}
