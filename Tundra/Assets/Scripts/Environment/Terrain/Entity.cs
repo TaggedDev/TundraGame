0b025077c127b2567bf9dd29c2c00ed319c0585f
taggeddev <denis.mikov.06@gmail.com>
2022-07-03 19:53:12 +0300

    feat(terrain): created new class representing entities

---
+++ Tundra/Assets/Scripts/Environment/Terrain/Entity.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+namespace Environment.Terrain
+{
+    public class Entity : MonoBehaviour
+    {
+        private Transform _player;
+        private Vector2 _entityPosition;
+
+        public void Initialise(Vector2 position, Transform player)
+        {
+            _entityPosition = position;
+            _player = player;
+        }
+
+        public void UpdateSelf()
+        {
+            Vector2 playerPosition = new Vector2(_player.position.x, _player.position.z);
+            if ((playerPosition - _entityPosition).sqrMagnitude <= 50f)
+            {
+                gameObject.SetActive(true);
+                return;
+            }
+            gameObject.SetActive(false);
+        }
+    }
+}
