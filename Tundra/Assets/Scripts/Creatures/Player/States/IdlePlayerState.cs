606c8ee110f1f235a4005538fb3363a820520ac2
taggeddev <denis.mikov.06@gmail.com>
2022-07-14 12:33:04 +0300

    refactor(naming): basic state renamed

---
+++ Tundra/Assets/Scripts/Creatures/Player/States/IdlePlayerState.cs
@@ -0,0 +1,31 @@
+﻿using Creatures.Player.Behaviour;
+using UnityEngine;
+
+namespace Creatures.Player.States
+{
+    public class IdlePlayerState : BasicPlayerState
+    {
+        public IdlePlayerState(PlayerMovement playerMovement, IPlayerStateSwitcher switcher) 
+            : base (playerMovement, switcher)
+        { }
+
+        private float _h = 0, _v = 0;
+
+        public override void MoveCharacter()
+        {
+            _h = Input.GetAxis("Horizontal");
+            _v = Input.GetAxis("Vertical");
+
+            if (Mathf.Abs(_h) > 0 || Mathf.Abs(_v) > 0)
+                PlayerStateSwitcher.SwitchState<WalkingPlayerState>();
+        }
+
+        public override void Start()
+        {
+            //_playerMovement.Animator.SetFloat("Speed", 0f);
+        }
+
+        public override void Stop()
+        { }
+    }
+}
