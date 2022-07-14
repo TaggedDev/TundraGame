c53f740fbacae272a330e0daf76eb6e2dcc6d075
taggeddev <denis.mikov.06@gmail.com>
2022-07-14 12:33:50 +0300

    refactor(naming): basic and idle state renamed

---
+++ Tundra/Assets/Scripts/Creatures/Player/States/BasicPlayerState.cs
@@ -0,0 +1,32 @@
+﻿using Creatures.Player.Behaviour;
+
+namespace Creatures.Player.States
+{
+    public abstract class BasicPlayerState
+    {
+        protected readonly PlayerMovement PlayerMovement;
+        protected readonly IPlayerStateSwitcher PlayerStateSwitcher;
+
+        protected BasicPlayerState(PlayerMovement playerMovement, IPlayerStateSwitcher switcher)
+        {
+            PlayerMovement = playerMovement;
+            PlayerStateSwitcher = switcher;
+    }
+
+        /// <summary>
+        /// On State changed | Start
+        /// </summary>
+        public abstract void Start();
+
+        /// <summary>
+        /// On State changed | Stop
+        /// </summary>
+        public abstract void Stop();
+
+        /// <summary>
+        /// Basic movement with sprint
+        /// </summary>
+        public abstract void MoveCharacter();
+    }
+
+}
