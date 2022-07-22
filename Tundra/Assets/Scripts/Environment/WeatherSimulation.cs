2509dac9dc7ebef97b992de3fc6205d2fb1a020e
taggeddev <denis.mikov.06@gmail.com>
2022-07-22 11:46:15 +0300

    refactor(weather): switched from IEnumarator to DeltaTime

--- Tundra/Assets/Scripts/Environment/WeatherSimulation.cs
+++ Tundra/Assets/Scripts/Environment/WeatherSimulation.cs
@@ -1,69 +1,67 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class WeatherSimulation : MonoBehaviour
+namespace Environment
 {
-    [SerializeField] private Transform player;
-    [SerializeField] private float EmissionMultiplyer;
-
-    public Vector3 Wind
+    public class WeatherSimulation : MonoBehaviour
     {
-        get
+        [SerializeField] private Transform player;
+        [SerializeField] private float EmissionMultiplyer;
+        [SerializeField] private float maxChangeWindDirectionCooldown;
+        
+        private float changeWindDirectionCooldown;
+        
+        public Vector3 Wind
         {
-            return _wind;
+            get
+            {
+                return _wind;
+            }
+            set
+            {
+                _wind = value;
+                WindUpdate();
+            }
         }
-        set
-        {
-            _wind = value;
-            WindUpdate();
-        }
-    }
 
 
-    private ParticleSystem _snowSystem;
-    private Vector3 _wind;
+        private ParticleSystem _snowSystem;
+        private Vector3 _wind;
 
 
-    IEnumerator GenerateRandomWind()
-    {
-        for (; ; )
+        private void GenerateRandomWind()
         {
             Wind = Vector3.ClampMagnitude(new Vector3(Random.Range(-100, 100), 0, Random.Range(-100, 100)), Random.Range(0, 16));
-            Debug.Log("WCh");
-            yield return new WaitForSeconds(Random.Range(70, 120));
+            changeWindDirectionCooldown = maxChangeWindDirectionCooldown;
         }
-    }
+        
+        private void Start()
+        {
+            _snowSystem = GetComponent<ParticleSystem>();
+            GenerateRandomWind();
+        }
 
+        private void Update()
+        {
+            transform.position = player.position;
+            changeWindDirectionCooldown -= Time.deltaTime;
+            if (changeWindDirectionCooldown <= 0)
+                GenerateRandomWind();
+        }
 
-    private void Start()
-    {
-        _snowSystem = GetComponent<ParticleSystem>();
-        StartCoroutine("GenerateRandomWind");
-    }
-
-    private void Update()
-    {
-
-    }
-
-
-
-
-
-    /// <summary>
-    /// updates snowflakes
-    /// </summary>
-    private void WindUpdate()
-    {
-        var vel = _snowSystem.velocityOverLifetime;
-        var emission = _snowSystem.emission;
-        var main = _snowSystem.main;
-        var collision = _snowSystem.collision;
-        vel.x = Wind.x != 0 ? new ParticleSystem.MinMaxCurve(Wind.x - 0.5f, Wind.x + 0.5f) : new ParticleSystem.MinMaxCurve(-1, 1);
-        vel.z = Wind.z != 0 ? new ParticleSystem.MinMaxCurve(Wind.z - 0.5f, Wind.z + 0.5f) : new ParticleSystem.MinMaxCurve(-1, 1);
-        emission.rateOverTime = Wind.magnitude * Wind.magnitude * EmissionMultiplyer + 10;
-        main.startLifetime = 100 - 4 * Wind.magnitude;
-        collision.lifetimeLoss = Wind.magnitude > 7 ? 1 : 0.95f;
+        /// <summary>
+        /// Updates snowflakes
+        /// </summary>
+        private void WindUpdate()
+        {
+            var vel = _snowSystem.velocityOverLifetime;
+            var emission = _snowSystem.emission;
+            var main = _snowSystem.main;
+            var collision = _snowSystem.collision;
+            vel.x = Wind.x != 0 ? new ParticleSystem.MinMaxCurve(Wind.x - 0.5f, Wind.x + 0.5f) : new ParticleSystem.MinMaxCurve(-1, 1);
+            vel.z = Wind.z != 0 ? new ParticleSystem.MinMaxCurve(Wind.z - 0.5f, Wind.z + 0.5f) : new ParticleSystem.MinMaxCurve(-1, 1);
+            emission.rateOverTime = Wind.magnitude * Wind.magnitude * EmissionMultiplyer + 10;
+            main.startLifetime = 100 - 4 * Wind.magnitude;
+            collision.lifetimeLoss = Wind.magnitude > 7 ? 1 : 0.95f;
+        }
     }
 }
