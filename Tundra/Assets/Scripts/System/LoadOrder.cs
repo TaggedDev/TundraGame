ef54c5d621ada9fa61eac355bab8e60139a6e120
taggeddev <denis.mikov.06@gmail.com>
2022-07-28 21:36:30 +0300

    rename(map): changed file name

--- Tundra/Assets/Scripts/System/LoadOrder.cs
+++ Tundra/Assets/Scripts/System/LoadOrder.cs
@@ -12,7 +12,7 @@ namespace System
         [SerializeField] private MapGenerator mapGenerator;
         [SerializeField] private ChunksGenerator chunksGenerator;
         [SerializeField] private PlayerSpawner playerHolder;
-        [SerializeField] private EntityGenerator entityGenerator;
+        [SerializeField] private EntityRenderer entityRenderer;
         [SerializeField] private MobFabric[] fabrics;
 
         /*
@@ -34,7 +34,7 @@ namespace System
             yield return new WaitUntil(() => mapGenerator.mapDataCount == 9 && mapGenerator.meshDataCount == 9);
             playerHolder.gameObject.SetActive(true);
             playerHolder.SpawnPlayer();
-            entityGenerator.gameObject.SetActive(true);
+            entityRenderer.gameObject.SetActive(true);
             foreach (var fabric in fabrics)
                 fabric.transform.gameObject.SetActive(true);
         }
