5d143b4048ec599e9a8374f7efc13ea49a7247ae
taggeddev <denis.mikov.06@gmail.com>
2022-09-19 19:29:41 +0300

    feat(scene): main menu buttons

---
+++ Tundra/Assets/Scripts/GUI/MainMenu/MainMenu_SceneSwitcher.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+namespace GUI.MainMenu
+{
+    public class MainMenu_SceneSwitcher : MonoBehaviour
+    {
+        /// <summary>
+        /// Is called when player clicks on 'Start Game' button
+        /// </summary>
+        public void OnStartGameBTNClicked()
+        {
+            Debug.Log("Switching to game creation scene");
+        }
+        
+        /// <summary>
+        /// Is called when player clicks on 'Load Game' button
+        /// </summary>
+        public void OnLoadGameBTNClicked()
+        {
+            Debug.Log("Switching to game loading scene");
+        }
+
+        /// <summary>
+        /// Is called when player clicks on 'Exit' button
+        /// </summary>
+        public void OnExitGameBTNClicked()
+        {
+            Debug.Log("Leaving game...");
+            Application.Quit();
+        }
+    }
+}
