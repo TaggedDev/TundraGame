741b2d239754de1df61aa9c25a14cfb7decc257a
IOExcept10n <56886020+IOExcept10n@users.noreply.github.com>
2022-09-23 11:46:13 +0500

    Concept has been changed, redo magic system

--- Tundra/Assets/Scripts/GUI/MagicPanelController.cs
+++ Tundra/Assets/Scripts/GUI/MagicPanelController.cs
@@ -10,22 +10,12 @@ using UnityEngine.UI;
 public class MagicPanelController : MonoBehaviour
 {
     private PlayerMagic _playerMagic;
-    private GameObject _segmentHolder;
-    private GameObject[] _draftElements;
-    private GameObject _spellIcon;
-    private int _selectedSegment;
 
     // Start is called before the first frame update
     void Start()
     {
         _playerMagic = UIController._rootCanvas.GetComponent<UIController>()._player.GetComponent<PlayerMagic>();
         _playerMagic.MagicPanelVisibilityChange += SwitchVisibility;
-        _segmentHolder = transform.Find("ElementSelectionCircle").Find("SegmentHolder").gameObject;
-        _draftElements = new GameObject[2];
-        _draftElements[0] = transform.Find("ElementSelectionCircle").Find("CenterCircle").Find("SelectedElementIcon1").gameObject;
-        _draftElements[1] = transform.Find("ElementSelectionCircle").Find("CenterCircle").Find("SelectedElementIcon2").gameObject;
-        _spellIcon = UIController._rootCanvas.transform.Find("SpellIcon").gameObject;
-        gameObject.SetActive(false);
     }
 
     private void SwitchVisibility(object sender, System.EventArgs e)
@@ -36,66 +26,9 @@ public class MagicPanelController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector3 mousePosition = Input.mousePosition - new Vector3(Screen.width / 2, Screen.height / 2);
-        float rot;
-        if (Mathf.Abs(mousePosition.x) < Mathf.Abs(mousePosition.y))
-        {
-            if (mousePosition.y > 0)
-            {
-                rot = 0;
-                _selectedSegment = 0;
-            }
-            else
-            { 
-                rot = 180;
-                _selectedSegment = 2;
-            }
-        }
-        else
-        {
-            if (mousePosition.x > 0)
-            {
-                rot = 270;
-                _selectedSegment = 3;
-            }
-            else
-            {
-                rot = 90;
-                _selectedSegment = 1;
-            }
-        }
-        _segmentHolder.transform.localRotation = Quaternion.Euler(new Vector3(0, 0, rot));
-        if (Input.GetMouseButtonDown(0))
-        {
-            if (_playerMagic.IsSpellingPanelOpened)
-            {
-                _playerMagic.AddElement(_selectedSegment);
-                print($"Element with index {_selectedSegment} has been added!");
-            }
-        }
     }
 
     private void FixedUpdate()
     {
-        for (int i = 0; i < 2; i++)
-        {
-            Color selectionColor = Color.gray;
-            switch (_playerMagic.DraftSpell[i])
-            {
-                case MagicElement.Fire:
-                    selectionColor = Color.red;
-                    break;
-                case MagicElement.Air:
-                    selectionColor = Color.white;
-                    break;
-                case MagicElement.Ground:
-                    selectionColor = new Color(1f, 0.6367924f, 0.6367924f);
-                    break;
-                case MagicElement.Water:
-                    selectionColor = Color.blue;
-                    break;
-            }
-            _draftElements[i].GetComponent<Image>().color = selectionColor;
-        }
     }
 }
