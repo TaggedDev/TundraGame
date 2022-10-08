b0c00b160879f154d49aa54b755af58a29d66db4
taggeddev <denis.mikov.06@gmail.com>
2022-10-08 10:29:45 +0300

    feat(scene): handling create world input

---
+++ Tundra/Assets/Scripts/GUI/CreateMenu_WorldGenerator.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace GUI
+{
+    public class CreateMenu_WorldGenerator : MonoBehaviour
+    {
+        [SerializeField] private Text worldNameInput;
+        [SerializeField] private Text worldSeedInput;
+
+        private void Start()
+        {
+            if (worldNameInput is null)
+                throw new Exception("World name input field is not assigned");
+            if (worldSeedInput is null)
+                throw new Exception("World seed input field is not assigned");
+        }
+
+        public void CreateWorld()
+        {
+            string worldName = worldNameInput.text;
+            int worldSeed = int.Parse(worldSeedInput.text);
+            Debug.Log($"{worldName}, {worldSeed}");
+        }
+    }
+}
