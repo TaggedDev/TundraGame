6ce50ce2cd365060d601d3b2c6f60872196b96e7
taggeddev <denis.mikov.06@gmail.com>
2022-10-28 20:13:10 +0300

    refactor(static): created FoodItem scriptableobject

---
+++ Tundra/Assets/Scripts/Creatures/Player/Inventory/ItemConfiguration/FoodItemConfiguration.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace Creatures.Player.Inventory.ItemConfiguration
+{
+    [CreateAssetMenu(fileName = "New Placeable Configuration", menuName = "Items/Food Configuration")]
+    public class FoodItemConfiguration : BasicItemConfiguration
+    {
+        
+    }
+}
