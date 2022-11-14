29be4dadf22d3c09db3c8c212366bcee7ca1930a
IOExcept10n <56886020+IOExcept10n@users.noreply.github.com>
2022-11-14 18:22:15 +0500

    Work on the craftbench panel

---
+++ Tundra/Assets/Scripts/GUI/Craft/CraftPanelUI.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GUI
+{
+    public class CraftPanelUI : MonoBehaviour
+    {
+        [SerializeField]
+        private GameObject recipePrefab;
+
+
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
