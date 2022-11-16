460db826132fa1fabab434b96a09b43a49c25221
IOExcept10n <56886020+IOExcept10n@users.noreply.github.com>
2022-11-16 23:06:28 +0500

    Update CrystalChannelScript.cs

--- Tundra/Assets/Scripts/Creatures/Player/Magic/CrystalChannelScript.cs
+++ Tundra/Assets/Scripts/Creatures/Player/Magic/CrystalChannelScript.cs
@@ -2,36 +2,40 @@
 using Creatures.Player.Magic;
 using UnityEngine;
 
-/// <summary>
-/// Script to control crystal channel spell.
-/// </summary>
-public class CrystalChannelScript : SpellScript<CrystalChannelSpell>
+namespace Creatures.Player.Magic
 {
-    private const double Time = 10;
-    private double _lifeTime;
-    private PlayerProperties _props;
 
-
-    private void Start()
+    /// <summary>
+    /// Script to control crystal channel spell.
+    /// </summary>
+    public class CrystalChannelScript : SpellScript<CrystalChannelSpell>
     {
-        _props = Configuration.Caster.GetComponent<PlayerProperties>();
-        _props.CurrentSpeed *= (float)Configuration.MovementFineCoefficient;
-    }
+        private const double Time = 10;
+        private double _lifeTime;
+        private PlayerProperties _props;
 
-    
-    private void Update()
-    {
-        gameObject.transform.position = Configuration.Caster.transform.position;
-        double coefficient = UnityEngine.Time.deltaTime;
-        _lifeTime += coefficient;
-        if (_lifeTime >= Time)
+
+        private void Start()
         {
-            Destroy(gameObject);
-            _props.CurrentSpeed /= (float)Configuration.MovementFineCoefficient;
+            _props = Configuration.Caster.GetComponent<PlayerProperties>();
+            _props.CurrentSpeed *= (float)Configuration.MovementFineCoefficient;
         }
-        // Heals player
-        _props.CurrentHealth += (float)(Configuration.Regeneration * coefficient);
-        if (_props.CurrentHealth > _props.MaxHealth) _props.CurrentHealth = _props.MaxHealth;
-        //TODO: deal damage to enemy which attacks player.
+
+
+        private void Update()
+        {
+            gameObject.transform.position = Configuration.Caster.transform.position;
+            double coefficient = UnityEngine.Time.deltaTime;
+            _lifeTime += coefficient;
+            if (_lifeTime >= Time)
+            {
+                Destroy(gameObject);
+                _props.CurrentSpeed /= (float)Configuration.MovementFineCoefficient;
+            }
+            // Heals player
+            _props.CurrentHealth += (float)(Configuration.Regeneration * coefficient);
+            if (_props.CurrentHealth > _props.MaxHealth) _props.CurrentHealth = _props.MaxHealth;
+            //TODO: deal damage to enemy which attacks player.
+        }
     }
 }
