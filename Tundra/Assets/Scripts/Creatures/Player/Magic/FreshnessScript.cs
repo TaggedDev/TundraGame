dd64cd6c342b601196b7ad010f0467e3c85f5520
IOExcept10n <56886020+IOExcept10n@users.noreply.github.com>
2022-11-16 23:01:20 +0500

    Update FreshnessScript.cs

--- Tundra/Assets/Scripts/Creatures/Player/Magic/FreshnessScript.cs
+++ Tundra/Assets/Scripts/Creatures/Player/Magic/FreshnessScript.cs
@@ -4,35 +4,39 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-/// <summary>
-/// A script to control freshness spell.
-/// </summary>
-public class FreshnessScript : SpellScript<FreshnessSpell>
+namespace Creatures.Player.Magic
 {
-    const double Time = 10;
-    double _lifeTime;
-    private PlayerProperties _props;
 
-    private void Start()
+    /// <summary>
+    /// A script to control freshness spell.
+    /// </summary>
+    public class FreshnessScript : SpellScript<FreshnessSpell>
     {
-        _props = Configuration.Caster.GetComponent<PlayerProperties>();
-        _props.CurrentSpeed *= (float)Configuration.PlayerSpeedCoefficient;
-    }
+        const double Time = 10;
+        double _lifeTime;
+        private PlayerProperties _props;
 
-    private void Update()
-    {
-        gameObject.transform.position = Configuration.Caster.transform.position;
-        double coefficient = UnityEngine.Time.deltaTime;
-        _lifeTime += coefficient;
-        if (_lifeTime >= Time)
+        private void Start()
         {
-            Destroy(gameObject);
-            _props.CurrentSpeed /= (float)Configuration.PlayerSpeedCoefficient;
+            _props = Configuration.Caster.GetComponent<PlayerProperties>();
+            _props.CurrentSpeed *= (float)Configuration.PlayerSpeedCoefficient;
         }
-        // Heals player
-        _props.CurrentHealth += (float)(Configuration.RegenerationCoefficient * coefficient);
-        if (_props.CurrentHealth > _props.MaxHealth) _props.CurrentHealth = _props.MaxHealth;
-        _props.CurrentWarmLevel += (float)(Configuration.PlayerWarmCoefficient * coefficient);
-        if (_props.CurrentWarmLevel > _props.MaxWarmLevel) _props.CurrentWarmLevel = _props.MaxWarmLevel;
+
+        private void Update()
+        {
+            gameObject.transform.position = Configuration.Caster.transform.position;
+            double coefficient = UnityEngine.Time.deltaTime;
+            _lifeTime += coefficient;
+            if (_lifeTime >= Time)
+            {
+                Destroy(gameObject);
+                _props.CurrentSpeed /= (float)Configuration.PlayerSpeedCoefficient;
+            }
+            // Heals player
+            _props.CurrentHealth += (float)(Configuration.RegenerationCoefficient * coefficient);
+            if (_props.CurrentHealth > _props.MaxHealth) _props.CurrentHealth = _props.MaxHealth;
+            _props.CurrentWarmLevel += (float)(Configuration.PlayerWarmCoefficient * coefficient);
+            if (_props.CurrentWarmLevel > _props.MaxWarmLevel) _props.CurrentWarmLevel = _props.MaxWarmLevel;
+        }
     }
 }
