aabd86203c3a07955582418c3ca9e2ac8eec4a8f
IOExcept10n <56886020+IOExcept10n@users.noreply.github.com>
2022-11-20 20:02:01 +0500

    Update PlaceableObjectBehaviour.cs

--- Tundra/Assets/Scripts/Creatures/Player/Crafts/Placeables/PlaceableObjectBehaviour.cs
+++ Tundra/Assets/Scripts/Creatures/Player/Crafts/Placeables/PlaceableObjectBehaviour.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 namespace Creatures.Player.Crafts
 {
+    /// <summary>
+    /// A script to handle placeable objects logic.
+    /// </summary>
     public class PlaceableObjectBehaviour : MonoBehaviour
     {
         [SerializeField]
@@ -11,8 +14,14 @@ namespace Creatures.Player.Crafts
         [SerializeField]
         private bool canBeOpened;
 
+        /// <summary>
+        /// A <see cref="PlaceableItemConfiguration"/> for this object.
+        /// </summary>
         public PlaceableItemConfiguration Configuration => configuration;
 
+        /// <summary>
+        /// Indicates if this object can be opened with an interaction key.
+        /// </summary>
         public bool CanBeOpened => canBeOpened;
     }
 }
