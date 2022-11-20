d48fa2accfbb0dd3290f406e5b8287d7245f12ff
IOExcept10n <56886020+IOExcept10n@users.noreply.github.com>
2022-11-20 21:53:18 +0500

    Update UIController.cs

--- Tundra/Assets/Scripts/GUI/HeadUpDisplay/UIController.cs
+++ Tundra/Assets/Scripts/GUI/HeadUpDisplay/UIController.cs
@@ -4,16 +4,36 @@ using UnityEngine;
 
 namespace GUI.HeadUpDisplay
 {
+    /// <summary>
+    /// A script to keep shared data of all HUD scripts.
+    /// </summary>
     public class UIController : MonoBehaviour
     {
-        [SerializeField] 
-        private GameObject player;
+        [SerializeField] private GameObject player;
 
+        /// <summary>
+        /// A <see cref="PocketCraftUI"/> instance.
+        /// </summary>
         public static PocketCraftUI PocketCraftUI { get; set; }
+
+        /// <summary>
+        /// A <see cref="CraftPanelUI"/> instance.
+        /// </summary>
         public static CraftPanelUI CraftPanel { get; set; }
+
+        /// <summary>
+        /// A HUD root canvas instance.
+        /// </summary>
         public static GameObject RootCanvas => RootUIInstance.gameObject;
 
+        /// <summary>
+        /// Instance of the root canvas sctipt.
+        /// </summary>
         public static UIController RootUIInstance { get; private set; }
+
+        /// <summary>
+        /// A player <see cref="GameObject"/> instance.
+        /// </summary>
         public GameObject Player { get => player; set => player=value; }
 
         private void Awake()
