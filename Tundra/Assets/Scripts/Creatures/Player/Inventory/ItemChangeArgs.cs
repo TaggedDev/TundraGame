6610c56f3445cf5630039af29421562ef0b7b62f
IOExcept10n <56886020+IOExcept10n@users.noreply.github.com>
2022-11-20 20:45:57 +0500

    Update ItemChangeArgs.cs

--- Tundra/Assets/Scripts/Creatures/Player/Inventory/ItemChangeArgs.cs
+++ Tundra/Assets/Scripts/Creatures/Player/Inventory/ItemChangeArgs.cs
@@ -2,11 +2,20 @@
 
 namespace Creatures.Player.Inventory
 {
+    /// <summary>
+    /// A class to provide event args to <see cref="InventoryContainer.ContentChanged"/> event etc.
+    /// </summary>
     public class ItemChangeArgs : EventArgs
     {
-        public int SlotID { get; private set; }
+        /// <summary>
+        /// Slot in which was an update of item.
+        /// </summary>
+        public int SlotID { get; }
 
-        public BasicItemConfiguration PreviousItem { get; private set; }
+        /// <summary>
+        /// Old item was in this sot before change.
+        /// </summary>
+        public BasicItemConfiguration PreviousItem { get; }
 
         public ItemChangeArgs(int slotID, BasicItemConfiguration previousItem)
         {
