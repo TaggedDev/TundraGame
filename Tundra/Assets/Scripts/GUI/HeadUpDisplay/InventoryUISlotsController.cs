e5b95699f1c587718a680be82ec6f052e767146e
taggeddev <denis.mikov.06@gmail.com>
2022-11-27 14:33:31 +0300

    refactor(slots): UI inventory slots are now instantiated with layout grid

--- Tundra/Assets/Scripts/GUI/HeadUpDisplay/InventoryUISlotsController.cs
+++ Tundra/Assets/Scripts/GUI/HeadUpDisplay/InventoryUISlotsController.cs
@@ -15,12 +15,11 @@ namespace GUI.HeadUpDisplay
         [SerializeField] private Sprite transparent;
         [SerializeField] private string pickupText;
         [SerializeField] private string openText;
-        [SerializeField] private RectTransform slotHolder;
+        private Camera _mainCamera;
         private PlayerInventory _inventoryController;
         private GameObject _player; // Replace GameObject with smth else
         private GameObject _pickupPanel; // same here
         private GameObject[] _visualSlots; // same here
-        private int _slotId = 1;
         private int _maxSlotsNumber;
         public float mouseScrollCoefficient = 10f;
         private Image _progressBar;
@@ -47,6 +46,7 @@ namespace GUI.HeadUpDisplay
         private void Start()
         {
             // Initialize all fields.
+            _mainCamera = Camera.main;
             // Bind to player StateChanged event
             _player = UIController.RootCanvas.GetComponent<UIController>().Player;
             _player.GetComponent<PlayerBehaviour>().StateChanged += (sender, args) =>
@@ -55,19 +55,10 @@ namespace GUI.HeadUpDisplay
                                     (sender as PlayerBehaviour).CurrentState is MagicCastingPlayerState);
                 gameObject.SetActive(visibility);
             };
-            // Prepare visual objects arrays.
-            _visualSlots = new GameObject[9];
-            _icons = new Image[9];
-            _texts = new Text[9];
-            for (int i = 0; i < 9; i++)
-            {
-                // Rework! No find method is allowed in runtime
-                _visualSlots[i] = GameObject.Find("InventorySlot" + (i + 1));
-                _icons[i] = _visualSlots[i].transform.Find("ItemIcon").gameObject.GetComponent<Image>();
-                _texts[i] = _visualSlots[i].transform.Find("AmountIndicator").gameObject.GetComponent<Text>();
-            }
+            
+            
 
-            // Cache another fields
+            /*// Cache another fields
             _inventoryController = _player.GetComponent<PlayerInventory>();
             _pickupPanel = GameObject.Find("ItemPickupPanel");
             _inventoryController.Inventory.MaxInventoryCapacityChanging += ResetSlots;
@@ -75,7 +66,7 @@ namespace GUI.HeadUpDisplay
             _progressBar = _pickupPanel.transform.Find("Progress").gameObject.GetComponent<Image>();
             _pickupLabel = _pickupPanel.transform.Find("Text").gameObject.GetComponent<Text>();
             // Reset slots values.
-            ResetSlots(this, _inventoryController.Inventory.MaxInventoryCapacity);
+            ResetSlots(this, _inventoryController.Inventory.MaxInventoryCapacity);*/
         }
 
         /// <summary>
@@ -85,9 +76,6 @@ namespace GUI.HeadUpDisplay
         /// <param name="e">Maximal slots number.</param>
         private void ResetSlots(object sender, int e)
         {
-            float newWidth = _inventoryController.Inventory.Slots.Length * 125 + 
-                             (_inventoryController.Inventory.Slots.Length - 1) * 25;
-            slotHolder.sizeDelta = new Vector2(100, 100);
             MaxSlotsNumber = e;
             float offset = 150 * UIController.RootCanvas.GetComponent<Canvas>().scaleFactor;
             var rect = (transform as RectTransform).rect;
@@ -108,7 +96,7 @@ namespace GUI.HeadUpDisplay
             }
         }
 
-        private void Update()
+        /*private void Update()
         {
             // TODO: Extract a function for user input
             
@@ -148,7 +136,7 @@ namespace GUI.HeadUpDisplay
                 _inventoryController.SelectedInventorySlot = tempVal;
             
             // Add new visual states
-            int i = 0;
+            /*int i = 0;
             foreach (var slot in _inventoryController.Inventory.Slots)
             {
                 GameObject UIslot = _visualSlots[i];
@@ -156,7 +144,7 @@ namespace GUI.HeadUpDisplay
                 _icons[i].sprite = slot.Item != null ? slot.Item.Icon : transparent;
                 _texts[i].text = slot.ItemsAmount.ToString();
                 i++;
-            }
+            }#1#
 
             // Show the nearest interactable item tile.
             if (_inventoryController.NearestInteractableItem == null)
@@ -167,11 +155,11 @@ namespace GUI.HeadUpDisplay
             {
                 // TODO: Add maincamera variable instead of calling the methods Camera.main
                 _pickupPanel.SetActive(true);
-                (_pickupPanel.transform as RectTransform).position = RectTransformUtility.WorldToScreenPoint(Camera.main, _inventoryController.NearestInteractableItem.transform.position) + new Vector2(0, 40);
+                (_pickupPanel.transform as RectTransform).position = RectTransformUtility.WorldToScreenPoint(_mainCamera, _inventoryController.NearestInteractableItem.transform.position) + new Vector2(0, 40);
                 _progressBar.fillAmount = _inventoryController.ItemPickingProgress / PlayerInventory.ItemPickingUpTime;
                 if (_inventoryController.NearestInteractableItem.GetComponent<DroppedItemBehaviour>()) _pickupLabel.text = pickupText;
                 else _pickupLabel.text = openText;
             }
-        }
+        }*/
     }
 }
---
+++ Tundra/Assets/Scripts/GUI/PlayerInventoryUI/UISlot.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace GUI.PlayerInventoryUI
+{
+    public class UISlot : MonoBehaviour
+    {
+        private Image _image;
+        
+        private void Start()
+        {
+            _image = GetComponent<Image>();
+        }
+    }
+}
